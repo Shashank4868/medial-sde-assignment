adfe601fa440035bb2c7ccf6da408fc3d4824f6a
Shashank4868 <shashankrajjais@gmail.com>
2024-08-12 20:38:16 +0530

    Update og-image link

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -2,55 +2,53 @@
 /* eslint-disable jsx-a11y/alt-text */
 // @ts-nocheck
 
-import { ImageResponse } from "next/og";
+import { ImageResponse } from 'next/og'
 
-export const runtime = "edge";
+export const runtime = 'edge'
 
 export async function GET(request: Request) {
   try {
-    const { searchParams } = new URL(request.url);
-    const title = searchParams.get("title");
-    const description = searchParams.get("description");
-    const imgUrl = searchParams.get("imgUrl");
-
-    // console.log(title, description);
+    const { searchParams } = new URL(request.url)
+    const title = searchParams.get('title')
+    const description = searchParams.get('description')
+    const imgUrl = searchParams.get('imgUrl')
 
     // Create a new Imageresponse(OG image) and return it.
 
     return new ImageResponse(
       (
-        <div tw="w-[1200px] h-[630px] text-white flex flex-col">
-          <div tw="flex relative">
+        <div tw='w-[1200px] h-[630px] text-white flex flex-col'>
+          <div tw='flex relative'>
             <img
               src={imgUrl}
-              alt="uploaded-image"
+              alt='uploaded-image'
               width={1200}
               height={630}
-              style={{ position: "relative" }}
+              style={{ position: 'relative' }}
             />
             <img
-              src="https://medial.app/image/medial-purple-logo.png"
-              alt="logo"
+              src='https://medial.app/image/medial-purple-logo.png'
+              alt='logo'
               style={{
-                position: "absolute",
-                width: "2rem",
-                height: "2rem",
-                marginTop: "2rem",
-                marginLeft: "2rem",
+                position: 'absolute',
+                width: '2rem',
+                height: '2rem',
+                marginTop: '2rem',
+                marginLeft: '2rem'
               }}
             />
           </div>
           <div
-            tw="flex absolute mt-[430px] h-[200px] flex-col w-full p-8 rounded-xl"
+            tw='flex absolute mt-[430px] h-[200px] flex-col w-full p-8 rounded-xl'
             style={{
-              backgroundColor: "rgba(255, 255, 255, 0.3)",
-              lineHeight: "2",
+              backgroundColor: 'rgba(255, 255, 255, 0.3)',
+              lineHeight: '2'
             }}
           >
-            <div tw="text-3xl relative text-red-700 text-center flex">
+            <div tw='text-3xl relative text-red-700 text-center flex'>
               {title}
             </div>
-            <div tw="text-md text-black relative font-semibold text-center flex">
+            <div tw='text-lg text-black relative font-semibold text-center flex'>
               {description}
             </div>
           </div>
@@ -58,11 +56,11 @@ export async function GET(request: Request) {
       ),
       {
         width: 1200,
-        height: 630,
+        height: 630
       }
-    );
+    )
   } catch (err: any) {
-    console.log(err);
-    return new Response("Failed to generate image", { status: 500 });
+    console.log(err)
+    return new Response('Failed to generate image', { status: 500 })
   }
 }
--- app/newpost/page.tsx
+++ app/newpost/page.tsx
@@ -3,16 +3,16 @@ import {
   CardContent,
   CardFooter,
   CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Metadata } from "next";
-import Image from "next/image";
+  CardTitle
+} from '@/components/ui/card'
+import { Metadata } from 'next'
+import Image from 'next/image'
 
-import logo from "@/app/assets/medial-purple-logo.png";
+import logo from '@/app/assets/medial-purple-logo.png'
 
 type Props = {
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+  searchParams: { [key: string]: string | string[] | undefined }
+}
 
 // generate dynamic metadata for the post
 
@@ -25,76 +25,76 @@ export const generateMetadata = ({ searchParams }: Props): Metadata => {
       description: `${searchParams.description}`,
       images: [
         {
-          url: `http://localhost:3000/api/og?title=${searchParams.title}&description=${searchParams.description}&imgUrl=${searchParams.imgUrl}`,
+          url: `/api/og?title=${searchParams.title}&description=${searchParams.description}&imgUrl=${searchParams.imgUrl}`,
           width: 1200,
           height: 630,
-          alt: `${searchParams.title}`,
-        },
-      ],
-    },
-  };
-};
+          alt: `${searchParams.title}`
+        }
+      ]
+    }
+  }
+}
 
 export default function Page({ searchParams }: Props) {
   // Show a post in a good UI
   return (
-    <div className="m-8">
+    <div className='m-8'>
       <Image
         src={logo}
-        alt="medial-logo"
+        alt='medial-logo'
         width={24}
-        style={{ height: "24px", margin: "2rem" }}
+        style={{ height: '24px', margin: '2rem' }}
       />
-      <p className="text-3xl font-semibold text-center m-8">New Post</p>
-      <Card className="m-auto w-full max-w-[400px] min-h-[500px] h-full rounded-3xl bg">
+      <p className='text-3xl font-semibold text-center m-8'>New Post</p>
+      <Card className='m-auto w-full max-w-[400px] min-h-[500px] h-full rounded-3xl bg'>
         <CardHeader>
-          <CardTitle className="text-left font-medium text-2xl  p-4">
+          <CardTitle className='text-left font-medium text-2xl  p-4'>
             Default user
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="w-[300] h-[300]">
+          <div className='w-[300] h-[300]'>
             <Image
               src={`${searchParams.imgUrl}`}
               alt={`${searchParams.title}`}
               width={400}
               height={300}
               style={{
-                margin: "auto",
-                borderRadius: "10px",
+                margin: 'auto',
+                borderRadius: '10px'
               }}
             />
           </div>
-          <div className="p-2 flex flex-row w-[150px] justify-between">
-            <div className="flex">
+          <div className='p-2 flex flex-row w-[150px] justify-between'>
+            <div className='flex'>
               <Image
-                width="24"
-                height="24"
-                src="https://img.icons8.com/material-outlined/24/FFFFFF/facebook-like--v1.png"
-                alt="facebook-like--v1"
+                width='24'
+                height='24'
+                src='https://img.icons8.com/material-outlined/24/FFFFFF/facebook-like--v1.png'
+                alt='facebook-like--v1'
               />
               {229}
             </div>
-            <div className="flex">
+            <div className='flex'>
               <Image
-                width="24"
-                height="24"
-                src="https://img.icons8.com/ios/50/FFFFFF/speech-bubble--v1.png"
-                alt="speech-bubble--v1"
+                width='24'
+                height='24'
+                src='https://img.icons8.com/ios/50/FFFFFF/speech-bubble--v1.png'
+                alt='speech-bubble--v1'
               />
               {178}
             </div>
           </div>
         </CardContent>
-        <CardFooter className="border-t flex flex-col rounded-xl text-left items-start">
-          <div className="text-left font-medium text-xl text-red-700 p-4 ">
+        <CardFooter className='border-t flex flex-col rounded-xl text-left items-start'>
+          <div className='text-left font-medium text-xl text-red-700 p-4 '>
             {searchParams.title}
           </div>
-          <p className="text-left w-full p-4 break-words text-md border-t">
+          <p className='text-left w-full p-4 break-words text-md border-t'>
             {searchParams.description}
           </p>
         </CardFooter>
       </Card>
     </div>
-  );
+  )
 }
